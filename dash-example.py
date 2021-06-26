a0f99bf6510cc339a1c287d47aebd8f7989d965f
Alex Johnstone <alexjj@gmail.com>
2021-06-26 14:28:38 +0100

    more dash tutorial

--- dash-example.py
+++ dash-example.py
@@ -6,32 +6,59 @@ import pandas as pd
 data = pd.read_csv('gotchi.csv')
 data['Value'] = data['BRS'] / data['Price (GHST)']
 
-app = dash.Dash(__name__)
+external_stylesheets = [
+    {
+        "href": "https://fonts.googleapis.com/css2?"
+                "family=Lato:wght@400;700&display=swap",
+        "rel": "stylesheet",
+    },
+]
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.title = "Aavegotchi Baazaar Explorer"
 
 app.layout = html.Div(
     children = [
-        html.H1(children="Aavegotchi Baazaar Explorer",),
+        html.Div(
+            children=[
+                html.P(children="👻📈", className='header-emoji'),
+        html.H1(
+            children="Aavegotchi Baazaar Explorer",
+            className='header-title',
+            ),
         html.P(
             children="Analyse past sales on the baazaar"
             " including as Gotchis, wearables and tickets"
-            " Future features include:"
-            " * Finding best value gotchi"
-            " * Market value of your gotchi"
-            " * Good deals based on historical prices",
+            " Future features include:",
+            className='header-description',),
+        html.P(
+            children=
+            "Finding best value gotchi,"
+            " Market value of your gotchi,"
+            " Good deals based on historical prices",),
+            ],
+            className='header',
         ),
-        dcc.Graph(
-            figure={
-                'data': [
-                    {
-                        "x": data['BRS'],
-                        "y": data['Price (GHST)'],
-                        "type": "scatter",
-                        'mode': 'markers',
-                    }
-                ],
-                'layout': {'title': 'Cost by Rarity'},
-            },
+        html.Div(
+            children=[
+                html.Div(
+                    children=dcc.Graph(
+                        figure={
+                            'data': [
+                                {
+                                    "x": data['BRS'],
+                                    "y": data['Price (GHST)'],
+                                    "type": "scatter",
+                                    'mode': 'markers',
+                                }
+                                    ],
+                            'layout': {'title': 'Cost by Rarity'},
+                            },
+                    ),
+                ),
+            ],
+            className="card",
         ),
+
         dcc.Graph(
             figure={
                 'data': [
