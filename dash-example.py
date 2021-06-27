4318a97526347545fc98fc26b25b03b09ebca7cf
Alex Johnstone <alexjj@gmail.com>
2021-06-27 12:22:15 +0100

    dash testing

--- dash-example.py
+++ dash-example.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 data = pd.read_csv('gotchi.csv')
 data['Value'] = data['BRS'] / data['Price (GHST)']
+data['Date'] = pd.to_datetime(data['Block Sold'], unit='s')
 
 external_stylesheets = [
     {
@@ -21,20 +22,21 @@ app.layout = html.Div(
         html.Div(
             children=[
                 html.P(children="👻📈", className='header-emoji'),
-        html.H1(
-            children="Aavegotchi Baazaar Explorer",
-            className='header-title',
-            ),
-        html.P(
-            children="Analyse past sales on the baazaar"
-            " including as Gotchis, wearables and tickets"
-            " Future features include:",
-            className='header-description',),
-        html.P(
-            children=
-            "Finding best value gotchi,"
-            " Market value of your gotchi,"
-            " Good deals based on historical prices",),
+                html.H1(
+                    children="Aavegotchi Baazaar Explorer",
+                    className='header-title',
+                    ),
+                html.P(
+                    children="Analyse past sales on the baazaar"
+                    " including as Gotchis, wearables and tickets"
+                    " Future features include:",
+                    className='header-description',),
+                html.P(
+                    children=
+                    "Finding best value gotchi,"
+                    " Market value of your gotchi,"
+                    " Good deals based on historical prices",
+                    className='header-description',),
             ],
             className='header',
         ),
@@ -42,36 +44,62 @@ app.layout = html.Div(
             children=[
                 html.Div(
                     children=dcc.Graph(
+                        id='price-chart',
+                        config={"displayModeBar": False},
                         figure={
                             'data': [
                                 {
                                     "x": data['BRS'],
                                     "y": data['Price (GHST)'],
+                                    'text': data['Name'],
                                     "type": "scatter",
                                     'mode': 'markers',
+                                    'hoverinfo': 'x+y+text',
+                                    'hovertemplate': 'BRS: %{x}<br>GHST: %{y}'
+                                                        "<extra>%{text}</extra>",
                                 }
-                                    ],
-                            'layout': {'title': 'Cost by Rarity'},
-                            },
+                            ],
+                            'layout': {
+                                'title': 'Cost by Rarity',
+                                "hovermode": "closest",
+                                'yaxis': {
+                                    'title': 'GHST (log scale)',
+                                    'type': 'log',
+                                }
+                                },
+                        },
                     ),
+                    className="card",
+                ),      
+                html.Div(
+                    children=dcc.Graph(
+                        id='value-chart',
+                        config={'displayModeBar': False},
+                        figure={
+                            'data': [
+                                {
+                                    "x": data['Date'],
+                                    "y": data['Value'],
+                                    "type": "lines",
+                                }
+                            ],
+                            'layout': {
+                                "title": "Cost efficiency of Gotchis",
+
+                            'yaxis': {
+                                'title': 'BRS/GHST',
+                                'range': [0, 2],
+                                },
+                            "colorway": ["#17B897"],
+                            },
+                        },                        
+                    ),
+                    className="card",
                 ),
             ],
-            className="card",
+            className="wrapper",
         ),
-
-        dcc.Graph(
-            figure={
-                'data': [
-                    {
-                        "x": data['Block Sold'],
-                        "y": data['Value'],
-                        "type": "lines",
-                    },
-                ],
-                "layout": {"title": "Cost efficiency of Gotchis"},
-            },
-        ),
-    ]
+    ],
 )
 
 if __name__ == "__main__":
